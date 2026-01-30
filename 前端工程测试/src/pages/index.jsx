7ea4f8bba48ab953f56d9979702a233664e0e737
Swm920307 <62643902+Swm920307@users.noreply.github.com>
2026-01-30 12:25:18 +0800

    feat: update components

---
+++ 前端工程测试/src/pages/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LearningPlatformDashboard from './components/LearningPlatformDashboard';
+
+const MyPageComponent = () => {
+  return <LearningPlatformDashboard />;
+};
+
+export default MyPageComponent;
